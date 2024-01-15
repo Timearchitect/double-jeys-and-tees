346889c78038f15369c623654a6f30a28f8ee2a0
Snorone <tobias.hurtig@gritacademy.se>
2024-01-15 14:34:23 +0100

    added api-key

---
+++ modules/fetch.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBN11hu8pjCdpykxWLyJb7mTFbPm0jBILI",
+
+  authDomain: "doublejeysandtees.firebaseapp.com",
+
+  databaseURL:
+    "https://doublejeysandtees-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "doublejeysandtees",
+
+  storageBucket: "doublejeysandtees.appspot.com",
+
+  messagingSenderId: "873418975332",
+
+  appId: "1:873418975332:web:abfbff74fbe4f7ae061771",
+};
