c03be76001d477bffbbe1dea4ba33af723a57697
Ton <146097352+wiangin@users.noreply.github.com>
2024-01-15 17:02:36 +0100

    add hamburger menu

--- script_main.js
+++ script_main.js
@@ -1,5 +1,21 @@
 import { putMessage, getMessages, deleteMessage } from "./modules/fetch.js";
 
+//Hamburger menu "Start"//
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', ()=>{
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', ()=>{
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+})
+)
+//Hamburger menu "End"//
+
 
 const formEL = document.querySelector('form');
 formEL.addEventListener('submit', (event) =>{
